f6310d309347eb6df38a661d7b6e1e9e4dcc3cbb
Natnat0905 <acidonathanielandrei@gmail.com>
2025-03-06 06:44:29 -0800

    OCR PLEASE FIX 1.18 17 is fixed but limited in file size

--- Procfile
+++
@@ -1,3 +0,0 @@
-﻿# Procfile
-
-web: bash setup.sh && uvicorn visual:app --host 0.0.0.0 --port $PORT
--- setup.sh
+++
@@ -1,6 +0,0 @@
-# setup.sh
-
-#!/bin/bash
-
-# Install Tesseract OCR and libtesseract-dev
-apt-get update && apt-get install -y tesseract-ocr libtesseract-dev
