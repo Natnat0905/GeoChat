352901879a3b709f4a9c89e7fc29d597b076ca4a
Natnat0905 <acidonathanielandrei@gmail.com>
2025-03-06 08:39:49 -0800

    OCR PLEASE FIX 1.23

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-# Install Tesseract OCR and its dependencies
+# Debugging output to log the commands and their results
+set -x  # Enables command tracing
+
+# Install necessary utilities
 apt-get update
-apt-get install -y tesseract-ocr libtesseract-dev libleptonica-dev
+apt-get install -y curl wget
+
+# Download and install Tesseract binary
+TESSERACT_VERSION="5.0.0-alpha.20210811"
+wget https://github.com/tesseract-ocr/tesseract/releases/download/$TESSERACT_VERSION/tesseract-$TESSERACT_VERSION-linux-x86_64.tar.gz
+tar -xzvf tesseract-$TESSERACT_VERSION-linux-x86_64.tar.gz
+cd tesseract-$TESSERACT_VERSION
+make
+make install
+
+# Verify the installation
+tesseract --version
